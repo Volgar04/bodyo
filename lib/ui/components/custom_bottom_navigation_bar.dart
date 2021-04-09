ac702264e51a7e11387e360e3b69c87c152909c7
Nicolas <nicolasmartin0404@gmail.com>
2021-04-09 19:01:37 +0200

    Custom navigation bar added

---
+++ lib/ui/components/custom_bottom_navigation_bar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 55.0,
+      width: double.infinity,
+      color: Colors.white,
+      child: Row(
+        children: [
+          NavigationBarItem(
+              text: 'Dashboard', imageAsset: 'assets/images/dashboard.png'),
+          NavigationBarItem(
+              text: 'Exercises', imageAsset: 'assets/images/kettlebell.png'),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 5.0, top: 2.0),
+              child: Image.asset('assets/images/home.png'),
+            ),
+          ),
+          NavigationBarItem(
+              text: 'Recipes',
+              imageAsset: 'assets/images/recipes.png',
+              isColorBlack: false),
+          NavigationBarItem(
+              text: 'Profile', imageAsset: 'assets/images/profile.png'),
+        ],
+      ),
+    );
+  }
+}
+
+class NavigationBarItem extends StatelessWidget {
+  final String imageAsset;
+  final String text;
+  final bool isColorBlack;
+
+  const NavigationBarItem(
+      {@required this.text,
+      @required this.imageAsset,
+      this.isColorBlack = true});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            imageAsset,
+            height: 30.0,
+            width: 30.0,
+          ),
+          Text(
+            text,
+            style: TextStyle(
+                color: isColorBlack ? Color(0xff161531) : Color(0xff20CAD1),
+                fontSize: 10.0,
+                fontWeight: FontWeight.w600),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bodyo/ui/components/custom_bottom_navigation_bar.dart';
 import 'package:bodyo/ui/components/recipes_banner.dart';
 import 'package:bodyo/ui/components/recipes_banner_tab_bar.dart';
 import 'package:bodyo/ui/components/recipes_header.dart';
@@ -24,26 +25,63 @@ class _HomeScreenState extends State<HomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            RecipesHeader(
-              title: 'Colorful pancakes',
-              background: 'assets/images/background.png',
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                NestedScrollView(
+                  headerSliverBuilder:
+                      (BuildContext context, bool innerBoxIsScrolled) {
+                    return <Widget>[
+                      RecipesHeader(
+                        title: 'Colorful pancakes',
+                        background: 'assets/images/background.png',
+                      ),
+                      RecipesBannerTabBar(
+                        banner: RecipesBanner(),
+                        tabBar: RecipesTabBar(tabController: _tabController),
+                      ),
+                    ];
+                  },
+                  body: TabBarView(
+                    controller: _tabController,
+                    children: [
+                      RecipesInstructionsTab(),
+                      RecipesIngredientsTab(),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: -1.0,
+                  right: 0.0,
+                  left: 0.0,
+                  child: Container(
+                    width: double.infinity,
+                    height: 40.0,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Theme.of(context).scaffoldBackgroundColor,
+                          Theme.of(context)
+                              .scaffoldBackgroundColor
+                              .withOpacity(0.7),
+                          Theme.of(context)
+                              .scaffoldBackgroundColor
+                              .withOpacity(0.1),
+                          // Colors.transparent,
+                        ],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            RecipesBannerTabBar(
-              banner: RecipesBanner(),
-              tabBar: RecipesTabBar(tabController: _tabController),
-            ),
-          ];
-        },
-        body: TabBarView(
-          controller: _tabController,
-          children: [
-            RecipesInstructionsTab(),
-            RecipesIngredientsTab(),
-          ],
-        ),
+          ),
+          CustomBottomNavigationBar(),
+        ],
       ),
     );
   }
